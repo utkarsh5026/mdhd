c781a01c6645e29e9f355c37b67410f3daafab9e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-15 00:26:34 +0530

    Remove unused category icon utility function to streamline the application and eliminate dead code.

--- app/src/components/utils/icon/category-icon.tsx
+++
@@ -1,6 +0,0 @@
-import getTopicIcon from "@/components/shared/icons/topicIcon";
-import type { Category } from "@/services/document";
-
-export const getCategoryIcon = (category: Category) => {
-  return getTopicIcon(category.path);
-};
