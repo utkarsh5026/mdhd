5fa53945f04396f707a24d017c4a89a52233fece
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-27 04:54:12 +0530

    Enhance Header component with GitHub button functionality
    - Updated the Header component to include a GitHub button that opens the repository in a new tab.
    - Improved button styling with hover effects for better user interaction and visual appeal.

--- app/src/components/layout/header.tsx
+++ app/src/components/layout/header.tsx
@@ -30,7 +30,14 @@ const Header = () => {
           </motion.div>
 
           <div className="flex items-center gap-4">
-            <Button variant="ghost" size="sm" className="gap-2">
+            <Button
+              variant="ghost"
+              size="sm"
+              className="gap-2 hover:bg-transparent rounded-full hover:text-primary hover:border-primary cursor-pointer hover:scale-105 transition-all duration-300"
+              onClick={() =>
+                window.open("https://github.com/utkarsh5026/mdhd", "_blank")
+              }
+            >
               <FaGithub className="w-4 h-4" />
               <span className="hidden md:inline">GitHub</span>
             </Button>
